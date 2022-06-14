d36e4bd2b1de48270397d59c0e4e379a00f8301b
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-06-14 17:26:35 +0530

    add cors policy

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const cookieParser = require('./middlewares/cookieParser');
 const formRoutes = require('./routes/formRoutes');
@@ -8,6 +9,7 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser);
 app.use('/forms', formRoutes);
 app.use('/auth', authRoutes);
